6b9d635a0bcbb3eb5deea135414ca93a9f40affb
atalavera21 <atalavera.0596@gmail.com>
2025-12-22 00:53:44 -0500

    fix: restore environment files with production values

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://analyzeai-api-cdd8hqdzc9f9hzak.centralus-01.azurewebsites.net/api',
+  googleClientId: '515503034164-c8umkosuk6hs7glgshbk5es63pq0uo5m.apps.googleusercontent.com'
+};
