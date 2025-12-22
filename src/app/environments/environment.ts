e1e2fd1f7639f5e5c4f975b2c4ec45574170ede2
atalavera21 <atalavera.0596@gmail.com>
2025-12-22 00:47:13 -0500

    fix: remove environment files from repo

--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5203/api',
-  googleClientId: '515503034164-c8umkosuk6hs7glgshbk5es63pq0uo5m.apps.googleusercontent.com'
-};
