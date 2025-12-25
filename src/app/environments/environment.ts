b4beec2e893ab74c8e7482d03af42da6232b2e8a
atalavera21 <atalavera.0596@gmail.com>
2025-12-25 18:40:16 -0500

    Agregación de nuevo archivo de configuración para el staticwebapp

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://analyzeai-api-cdd8hqdzc9f9hzak.centralus-01.azurewebsites.net/api',
+  // apiUrl: 'https://analyzeai-api-cdd8hqdzc9f9hzak.centralus-01.azurewebsites.net/api',
+  apiUrl: 'http://localhost:5203/api',
   googleClientId: '515503034164-c8umkosuk6hs7glgshbk5es63pq0uo5m.apps.googleusercontent.com'
 };
